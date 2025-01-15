2fb40551f6ef177e6f4cfb6fa3e1ca6b261c18d4
mercyogalo <143324456+mercyogalo@users.noreply.github.com>
2025-01-15 22:35:53 +0300

    updated the pages
    added the base file and started jinja templating

--- electricwebsite/shop_features/urls.py
+++ electricwebsite/shop_features/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from . import views
+
+
+
+app_name="shop_features"
 
 urlpatterns = [
-    path("home", views.home,name="home"),
+    path("", views.home,name="home"),
 ]
