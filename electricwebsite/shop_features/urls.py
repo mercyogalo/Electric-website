4dc1a29a7f8b2e17dc308fec28e96723de04d0ad
mercyogalo <143324456+mercyogalo@users.noreply.github.com>
2025-01-17 23:30:52 +0300

    updated the links
    added the functions for the html files

--- electricwebsite/shop_features/urls.py
+++ electricwebsite/shop_features/urls.py
@@ -7,4 +7,7 @@ app_name="shop_features"
 
 urlpatterns = [
     path("", views.home,name="home"),
+    path("checkout/", views.checkout, name="checkout"),
+    path("product/", views.product, name="product"),
+    path("store/", views.store, name="store"),
 ]
--- electricwebsite/shop_features/views.py
+++ electricwebsite/shop_features/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'index.html')
+
+def store(request):
+    return render(request,'store.html')
+
+def product(request):
+    return render(request,'product.html')
+
+def checkout(request):
+    return render(request,'checkout.html')
