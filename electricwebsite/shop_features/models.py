e8d21015ed41feb4bfa1ba1adaf66780eeb1da72
mercyogalo <143324456+mercyogalo@users.noreply.github.com>
2025-01-20 18:05:11 +0300

    added the model for products
    Added the model to add products

--- electricwebsite/shop_features/models.py
+++ electricwebsite/shop_features/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Name(models.Model):
+    name=models.CharField(max_length=100)
+    price=models.IntegerField()
+    category=models.CharField(max_length=100)
+    
+
+    def __str__(name):
+        return 
+
+   
