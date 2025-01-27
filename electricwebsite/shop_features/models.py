62123dcd7d3b2a44f531086e5d9720cf9930146e
mercyogalo <143324456+mercyogalo@users.noreply.github.com>
2025-01-27 22:36:13 +0300

    updated the models
    updated the models

--- electricwebsite/shop_features/models.py
+++ electricwebsite/shop_features/models.py
@@ -1,4 +1,5 @@
-from django.db import models, datetime
+from django.db import models
+import datetime
 
 # Create your models here.
 
@@ -20,11 +21,25 @@ class Customer(models.Model):
         return  {self.first_name}+{self.last_name}
     
     
+    
+    
+    
+class Product(models.Model):
+    name=models.CharField(max_length=100)
+    price=models.IntegerField()
+    category=models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    description=models.CharField( max_length=250, default="", blank=True, null=True)
+    image=models.ImageField( upload_to='productimage/')
+    
+
+    def __str__(self):
+        return self.name    
+    
 class Order(models.Model):
     product=models.ForeignKey(Product, on_delete=models.CASCADE )
     Customer=models.ForeignKey(Customer, on_delete=models.CASCADE)
     quantity=models.IntegerField(default=1)
-    address=models.CharField( max_length=100, default="", blank=TRUE)
+    address=models.CharField( max_length=100, default="", blank=True)
     phone=models.CharField(max_length=100, default="", blank=True, null=True)
     status=models.BooleanField(default="false")
     date=models.DateField(default=datetime.datetime.today)
@@ -39,15 +54,6 @@ class Order(models.Model):
 
 
 
-class Product(models.Model):
-    name=models.CharField(max_length=100)
-    price=models.IntegerField()
-    category=models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
-    description=models.CharField( max_length=250, default="", blank=TRUE, null=TRUE)
-    image=models.ImageField( upload_to='productimage/')
-    
 
-    def __str__(self):
-        return self.name
 
     
