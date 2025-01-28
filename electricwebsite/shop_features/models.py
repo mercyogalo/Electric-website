306e116d595a91c62722ef1711b8165ba1ee2fa9
mercyogalo <143324456+mercyogalo@users.noreply.github.com>
2025-01-28 12:57:43 +0300

    updated the product model
    added the is sale functionality to determine all the sales products

--- electricwebsite/shop_features/models.py
+++ electricwebsite/shop_features/models.py
@@ -26,10 +26,12 @@ class Customer(models.Model):
     
 class Product(models.Model):
     name=models.CharField(max_length=100)
-    price=models.IntegerField()
+    price=models.IntegerField(default=0)
     category=models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     description=models.CharField( max_length=250, default="", blank=True, null=True)
     image=models.ImageField(upload_to='productimage/' , default='default.jpg')
+    is_sale=models.BooleanField(default=False)
+    sale_price=models.IntegerField(default=0)
     
 
     def __str__(self):
