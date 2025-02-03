eff6e02ed556fa04b81e92a2fd5cea14781e9b47
mercyogalo <143324456+mercyogalo@users.noreply.github.com>
2025-02-03 15:57:35 +0300

    updated the cart_delete function
    updated the code to delete products on click

--- electricwebsite/shop_features/views.py
+++ electricwebsite/shop_features/views.py
@@ -124,4 +124,16 @@ def cart_update(request):
     pass
 
 def cart_delete(request):
-    pass
+    cart = Cart(request)
+    
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        
+        cart.delete(product=product_id)
+        
+        response = JsonResponse({'product': product_id})
+    
+        return response
+        
+        
+    
