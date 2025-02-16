8db15d65f02dacbcacffc967436817e15d07aaa8
mercyogalo <143324456+mercyogalo@users.noreply.github.com>
2025-02-16 15:47:21 +0300

    added the model for shipping
    created the shipping model

--- electricwebsite/shop_features/models.py
+++ electricwebsite/shop_features/models.py
@@ -19,7 +19,7 @@ class Customer(models.Model):
     
     
     def __str__(self):
-        return  {self.first_name}+{self.last_name}
+        return  f'{self.first_name} {self.last_name}'
     
     
     
