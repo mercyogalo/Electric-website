d3fbd2d2e9ea6ce6d358fdde13637f61b408fc64
mercyogalo <143324456+mercyogalo@users.noreply.github.com>
2025-02-19 11:11:52 +0300

    removed the payment function

--- electricwebsite/payment/views.py
+++ electricwebsite/payment/views.py
@@ -3,29 +3,8 @@ from django.contrib import messages
 from cart.cart import Cart
 from .forms import ShippingForm
 from .models import ShippingAddress
-from django.contrib.auth.decorators import login_required  
 
-@login_required  
+
 def shipping(request):
-    cart = Cart(request)
-    cart_products = cart.get_prods
-    quantities = cart.get_quants
-
-
-    shipping_user = ShippingAddress.objects.filter(user=request.user).first()
-
-
-    shipping_form = ShippingForm(request.POST or None, instance=shipping_user)
-
-    if request.method == "POST" and shipping_form.is_valid():
-        shipping_form.save()
-        messages.success(request, "Your Shipping Information Has Been Updated!")
-        return redirect('home')
-
-    context = {
-        'cart_products': cart_products,
-        'quantities': quantities,
-        'shipping_form': shipping_form,
-    }
-
+    context = {}
     return render(request, 'shop_features/shipping.html', context)
